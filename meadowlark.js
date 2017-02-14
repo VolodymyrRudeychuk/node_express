23ee8f12a22c38395c402d109a4256a92b11dcbf
VolodymyrRudeychuk <vladrudeycuk@gmail.com>
2017-02-14 12:53:52 +0200

    first commit

---
+++ meadowlark.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+// Установка механизма представления handlebars
+var handlebars = require('express-handlebars')
+    .create({ defaultLayout:'main' });
+var fortune = require('./lib/fortune.js');
+
+app.engine('handlebars', handlebars.engine);
+
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+// home
+app.get('/', function(req, res){
+    res.render('home')
+})
+
+//about
+app.get('/about', function(req, res){
+    res.render('about', { fortune: randomFortune });
+})
+
+// 404
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404')
+});
+
+// 500
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500')
+});
+
+app.listen(app.get('port'), function(){
+    console.log( 'Express запущен на http://localhost:' +
+    app.get('port') + '; нажмите Ctrl+C для завершения.' );
+});
